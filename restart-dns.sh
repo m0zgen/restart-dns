e274b0234e0a5229480d64f67a69319f8f55e8ed
root <root@pc030.rb.kz>
2016-04-05 10:03:58 +0600

    Add check nscd installed function

--- restart-dns.sh
+++ restart-dns.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
+
+_NSCD="nscd"
+
+if [[ "`systemctl is-active $_NSCD`" == "unknown" ]]; then
+	#statements
+	echo -e "\nService $_NSCD not installed, install from dnf...\n"
+	dnf -y install $_NSCD && systemctl enable $_NSCD && systemctl start $_NSCD
+fi
+
+echo -e "\nRestarting services.."
 systemctl restart NetworkManager && nscd -i hosts && systemctl restart nscd
+
+echo -e "Complete!"
